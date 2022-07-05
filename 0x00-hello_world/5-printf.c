b43c35af87624c04bb0f0a1b910c868f86162468
edexty <ederhitedestiny841@gmail.com>
2022-07-05 11:11:09 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,4 @@
  *my hello, printf task
  *Return: return 1 after program execution
  */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-}
+int main
